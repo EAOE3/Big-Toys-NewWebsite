a355f7efd2016ce9ce7cb5b9816c671a6411aa03
loque <madridx2010@gmail.com>
2021-10-18 10:32:38 -0500

    changed discord links to permanent links again

--- src/components/commons/footer/index.js
+++ src/components/commons/footer/index.js
@@ -16,7 +16,7 @@ const footer = props => {
                         </span>
                     </a>
                     &nbsp;&nbsp;
-                    <a  href="https://discord.gg/dB7NuXBn9Z" target="_blank">
+                    <a  href="https://discord.gg/AtUaEehXJe" target="_blank">
                         <span className="icon has-text-white is-size-4">
                             <i class="fab fa-discord"></i>
                         </span>
